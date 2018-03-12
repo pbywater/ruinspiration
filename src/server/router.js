c3f3c24f0b8f1460e480122b84facdfde9571e5a
pbywater <philippabywater@gmail.com>
2018-03-12 13:16:13 +0000

    set up

--- src/server/router.js
+++ src/server/router.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
 require('env2')('./config.env');
-
-const { checkSignIn } = require('./../helpers/helpers.js');
 
 const router = express.Router();
 
@@ -11,19 +8,6 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true,
 }));
-
-const session = cookieSession({
-  name: 'authenticated',
-  secret: process.env.SESSION_SECRET,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    signed: true,
-    maxAge: 1 * 60 * 60 * 1000, // 1 hour
-  },
-});
-
-router.use(session);
 
 router.get('/', require('./controllers/home.js'));
 
